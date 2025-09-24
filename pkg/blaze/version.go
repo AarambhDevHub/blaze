4f4248beb3e50adbc25dca8f18dd79b94a5f40ea
Darshan vichhi <darshanvichhi111@gmail.com>
2025-09-24 10:58:33 +0530

    version

---
+++ pkg/blaze/version.go
@@ -0,0 +1,17 @@
+package blaze
+
+const (
+	Version   = "1.0.0"
+	BuildDate = "2025-09-24"
+	GoVersion = "1.24.0"
+)
+
+// GetVersion returns framework version info
+func GetVersion() map[string]string {
+	return map[string]string{
+		"version":    Version,
+		"build_date": BuildDate,
+		"go_version": GoVersion,
+		"framework":  "Blaze",
+	}
+}
